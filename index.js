9e157f9814792670b6cc64178cd3b4aacd0e472a
Herbert Henrique <herbertmatos10@hotmail.com>
2022-11-22 22:59:44 -0300

    adicona funcao para remover tags html de uma string e funcao para mapear itens html e retonar objeto com dados processados

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
 
-const main = async () => {
-  const response = await fetch('https://scholar.google.com.br/scholar?hl=pt-BR&as_sdt=0%2C5&q=intro+a+computer+science&btnG=');
-  const body = await response.text();
-
-  const dom = new JSDOM(body)
-  dom.window.document.querySelectorAll('.gs_rt a').forEach(item => console.log({
-    title: item.textContent,
-    link: item.getAttribute('href')
-  }))
+function removeHtmlTags(text) {
+  const regex = /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
+  return text.replaceAll(regex, '');
 }
 
+function mapSearchResult(data) {
+  const dataMapped = [];
+  for (const item of data) {
+    const title = item.querySelector('.gs_rt')?.innerHTML || "";
+    const link = item.querySelector('h3.gs_rt a')?.getAttribute('href') || "";
+    const authors = item.querySelector('.gs_a')?.innerHTML || "";
+    const description = item.querySelector('div.gs_rs')?.innerHTML || "";
+    dataMapped.push({
+      title: removeHtmlTags(title),
+      authors: removeHtmlTags(authors),
+      description: removeHtmlTags(description),
+      link
+    }); 
+  }
+  return dataMapped;
+}
+
+const main = async () => {
+  const response = await fetch('https://scholar.google.com.br/scholar?hl=pt-BR&as_sdt=0%2C5&q=intro+a+computer+science&btnG=');
+  const responseBody = await response.text();
+  const dom = new JSDOM(responseBody);
+  const body = dom.window.document.querySelector('body');
+  const searchResultItens = body.querySelectorAll('.gs_r');
+  console.log(mapSearchResult(searchResultItens));
+}
 main();
